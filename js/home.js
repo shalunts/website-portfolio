034812a1f8047d4dbdba67df7c2555447b82f2bf
shalunts <hayk.shalunts@gmail.com>
2017-04-10 13:18:32 -0700

    footer

--- js/home.js
+++ js/home.js
@@ -74,3 +74,48 @@ function svgReset() {
 
 }
       // SVG color picking end
+
+
+
+      jQuery(function($)
+      {
+          $("#contact_form").submit(function()
+          {
+              var email = $("#email").val(); // get email field value
+              var name = $("#name").val(); // get name field value
+              var msg = $("#msg").val(); // get message field value
+              $.ajax(
+              {
+                  type: "POST",
+                  url: "https://mandrillapp.com/api/1.0/messages/send.json",
+                  data: {
+                      'key': 'sVRLFidC1A7K56TuUkyUQg',
+                      'message': {
+                          'from_email': email,
+                          'from_name': name,
+                          'headers': {
+                              'Reply-To': email
+                          },
+                          'subject': 'Website Contact Form Submission',
+                          'text': msg,
+                          'to': [
+                          {
+                              'email': 'hayk.shalunts@gmail.com',
+                              'name': 'Hayk Shalunts',
+                              'type': 'to'
+                          }]
+                      }
+                  }
+              })
+              .done(function(response) {
+                  alert('Your message has been sent. Thank you!'); // show success message
+                  $("#name").val(''); // reset field after successful submission
+                  $("#email").val(''); // reset field after successful submission
+                  $("#msg").val(''); // reset field after successful submission
+              })
+              .fail(function(response) {
+                  alert('Error sending message.');
+              });
+              return false; // prevent page refresh
+          });
+      });
